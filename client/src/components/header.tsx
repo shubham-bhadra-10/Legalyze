659ae31e33ce442a5f0f373f75b9b26a115617c4
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-08 11:00:25 -0400

    Added navItems

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,5 +1,10 @@
 import Link from 'next/link';
 
+const navItems: { name: string; href: string }[] = [
+  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Pricing', href: '/pricing' },
+  { name: 'Privacy Policy', href: '/privacy' },
+];
 export function Header() {
   return (
     <header className='sticky ps-4 top-0 z-50 w-full border-b bg-background/95 backdrop-blur'>
