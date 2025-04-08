27c80b5c13a3bdc01196c73d35b0b74286f2a99b
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-08 10:58:39 -0400

    Added components folder and header component

---
+++ client/src/components/header.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header className='sticky ps-4 top-0 z-50 w-full border-b bg-background/95 backdrop-blur'>
+      <div className='container flex h-16 items-center'>
+        <div className='mr-4 hidden md:flex'>
+          <Link href='/' className='mr-6 flex items-center space-x-2'>
+            Legalyze
+          </Link>
+        </div>
+      </div>
+      header
+    </header>
+  );
+}
