017f8737a9e0fbfaeb830af8a66c261fb7cd0cce
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-08 17:08:21 -0400

    Added logout router

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -24,4 +24,13 @@ router.get('/current-user', (req, res) => {
   }
 });
 
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).json({ message: 'Logged out successfully' });
+  });
+});
+
 export default router;
