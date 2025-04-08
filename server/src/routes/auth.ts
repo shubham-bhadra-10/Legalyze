2cdf3313c66e9dcff3f4c6942f4c9186663e3bd5
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-08 12:46:53 -0400

    Added authorization route into /routes/auth

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -16,4 +16,12 @@ router.get(
   }
 );
 
+router.get('/current-user', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
+
 export default router;
