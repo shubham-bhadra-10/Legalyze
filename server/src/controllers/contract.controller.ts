41618bfce6a99966bacc3b52c9d383b7d87a3217
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-09 08:10:13 -0400

    Created upload feature for contracts using Multer package

---
+++ server/src/controllers/contract.controller.ts
@@ -0,0 +1,11 @@
+import multer from 'multer';
+const upload = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+      return cb(null, true);
+    }
+    cb(null, false);
+    cb(new Error('Only .pdf format allowed!'));
+  },
+}).single('contract');
