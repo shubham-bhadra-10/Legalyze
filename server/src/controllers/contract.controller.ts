4d54d5294eeec32e6c12cdfe95ae2dd8f12c9812
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-09 08:23:15 -0400

    Updated contract controller

--- server/src/controllers/contract.controller.ts
+++ server/src/controllers/contract.controller.ts
@@ -4,8 +4,9 @@ const upload = multer({
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'application/pdf') {
       return cb(null, true);
+    } else {
+      cb(null, false);
+      cb(new Error('Only .pdf format allowed!'));
     }
-    cb(null, false);
-    cb(new Error('Only .pdf format allowed!'));
   },
 }).single('contract');
