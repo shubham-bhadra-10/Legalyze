f10fa381a6d5611b05c459027b669b1fcae7ccfc
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-09 10:08:54 -0400

    Utilized Gemini Pro to identify contract type

--- server/src/services/ai.services.ts
+++ server/src/services/ai.services.ts
@@ -1,19 +1,19 @@
-// service to extract text from pdf
-
 import redis from '../controllers/redis';
 import { getDocument } from 'pdfjs-dist';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const AI_MODEL = 'gemini-pro';
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY!);
 
 export const extractTextFromPdf = async (fileKey: string) => {
   try {
     const fileData = await redis.get(fileKey);
-    if (!fileData) {
-      throw new Error('File not found in Redis');
-    }
+    if (!fileData) throw new Error('File not found in Redis');
+
     let fileBuffer: Uint8Array;
     if (Buffer.isBuffer(fileData)) {
       fileBuffer = new Uint8Array(fileData);
     } else if (typeof fileData === 'object' && fileData !== null) {
-      // check if the object has the expected structure
       const bufferData = fileData as { type?: string; data?: number[] };
       if (bufferData.type === 'Buffer' && Array.isArray(bufferData.data)) {
         fileBuffer = new Uint8Array(bufferData.data);
@@ -23,17 +23,32 @@ export const extractTextFromPdf = async (fileKey: string) => {
     } else {
       throw new Error('Invalid file data format in Redis');
     }
+
     const pdf = await getDocument({ data: fileBuffer }).promise;
     let text = '';
+
     for (let i = 1; i <= pdf.numPages; i++) {
       const page = await pdf.getPage(i);
       const content = await page.getTextContent();
       const textItems = content.items.map((item: any) => item.str);
       text += textItems.join(' ') + '\n';
     }
+
     return text;
   } catch (error) {
     console.error('Error extracting text from PDF:', error);
     throw new Error('Failed to extract text from PDF');
   }
+};
+
+export const detectContractType = async (
+  contractText: string
+): Promise<string> => {
+  const model = genAI.getGenerativeModel({ model: AI_MODEL });
+  const prompt = `Analyze the following contract text and deteremine the type of contract it is. Provide only type as a single string (e.g. "Employment", "Non-Disclosure Agreement", "Sales","Lease",etc.). Do not include any additional explaination or text.
+  
+  Contract Text: ${contractText.substring(0, 2000)}`;
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  return response.text().trim();
 };
