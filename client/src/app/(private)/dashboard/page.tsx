0f48b55e4d81137ee27411d9451b25c5c39e8a3e
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-09 14:35:18 -0400

    small updates

---
+++ client/src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Dashboard() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <Button>Upload Contract</Button>
+    </div>
+  );
+}
