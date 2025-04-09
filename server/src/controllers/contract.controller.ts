ab6bda5fb46c80643994b0edf30ce84388fae51f
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-09 09:40:47 -0400

    created function to confirm and detect contract type

--- server/src/controllers/contract.controller.ts
+++ server/src/controllers/contract.controller.ts
@@ -1,4 +1,10 @@
 import multer from 'multer';
+import { Request, Response } from 'express';
+
+import { IUser } from '../models/user.model';
+import '../types/express'; // Import the extended Request type for user property
+import redis from './redis';
+import { extractTextFromPdf } from '../services/ai.services';
 const upload = multer({
   storage: multer.memoryStorage(),
   fileFilter: (req, file, cb) => {
@@ -10,3 +16,25 @@ const upload = multer({
     }
   },
 }).single('contract');
+
+export const detectAndConfirmContractType = async (
+  req: Request,
+  res: Response
+) => {
+  const user = req.user as IUser;
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+  try {
+    const fileKey = `file:${user._id}:${Date.now()}`;
+    await redis.set(fileKey, req.file.buffer);
+    await redis.expire(fileKey, 60 * 60); // Set expiration to 1 hour
+    const pdfText = await extractTextFromPdf(fileKey);
+    return res.status(200).json({ text: pdfText });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error processing PDF',
+      error: (error as Error).message,
+    });
+  }
+};
