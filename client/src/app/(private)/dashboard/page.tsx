44801040dc55baf29d9d8f5eb84198884e5ca583
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-10 15:34:09 -0400

    Updated to solve rendering bug

--- client/src/app/(private)/dashboard/page.tsx
+++ client/src/app/(private)/dashboard/page.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import UploadModel from '@/components/models/upload-model';
 import { Button } from '@/components/ui/button';
+import { Upload } from 'lucide-react';
+import { useState } from 'react';
 
 export default function Dashboard() {
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+
   return (
     <div className='flex flex-col items-center justify-center'>
-      <Button>Upload Contract</Button>
+      <Button onClick={() => setIsUploadModalOpen(true)}>
+        <Upload className='mr-2 h-4 w-4' />
+        Upload Contract
+      </Button>
+
+      <UploadModel
+        isOpen={isUploadModalOpen}
+        onClose={() => setIsUploadModalOpen(false)}
+        onUploadComplete={() => setIsUploadModalOpen(false)}
+      />
     </div>
   );
 }
