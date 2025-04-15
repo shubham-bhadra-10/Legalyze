d6ea6db38c053d1acaec56d68522f2419506367f
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-15 10:24:57 -0400

    added UserContracts in dashboard page

--- client/src/app/(private)/dashboard/page.tsx
+++ client/src/app/(private)/dashboard/page.tsx
@@ -1,25 +1,18 @@
 'use client';
 
+import UserContracts from '@/components/dashboard/user-contracts';
 import UploadModel from '@/components/models/upload-model';
 import { Button } from '@/components/ui/button';
-import { Upload } from 'lucide-react';
+import { Upload, User } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Dashboard() {
   const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <Button onClick={() => setIsUploadModalOpen(true)}>
-        <Upload className='mr-2 h-4 w-4' />
-        Upload Contract
-      </Button>
-
-      <UploadModel
-        isOpen={isUploadModalOpen}
-        onClose={() => setIsUploadModalOpen(false)}
-        onUploadComplete={() => setIsUploadModalOpen(true)}
-      />
+    <div>
+      <UserContracts />
+      
     </div>
   );
 }
