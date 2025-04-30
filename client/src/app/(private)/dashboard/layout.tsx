46e1676c8cd766ef1dff02e08eebc9add8f705a5
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-30 07:00:36 -0400

    Added card to authenticate when logout

--- client/src/app/(private)/dashboard/layout.tsx
+++ client/src/app/(private)/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { ProtectedLayout } from "@/components/dashboard/protected-layout";
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
         <ProtectedLayout>
-            <main>{children}</main>
+            <main className="flex-1 overflow-x-hidden bg-white overflow-y-auto">{children}</main>
         </ProtectedLayout>
     )
 }
