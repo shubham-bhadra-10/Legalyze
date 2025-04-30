aaada66a0abac561796e6e411fc36b5b1f6ed547
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-04-30 06:13:44 -0400

    protected layout created

---
+++ client/src/components/dashboard/protected-layout.tsx
@@ -0,0 +1,22 @@
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { Loader2 } from "lucide-react";
+
+export function ProtectedLayout({ children }: {children: React.ReactNode}) {
+    const { user,isLoading } = useCurrentUser();
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <div className="flex items-center justify-center">
+                    <Loader2 className="size-4 mr-2 animate-spin" />
+                </div>
+            </div>
+        );
+    }
+    if (!user){
+        return(
+            <div></div>
+        )
+    }
+        
+    }
+
